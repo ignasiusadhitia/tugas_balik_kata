5516bbb132f30f13fc09cc22079fffd4180ab2a4
Ignasius Yuda Adhitia <ignasius.yuda.adhitia@gmail.com>
2021-12-22 17:14:08 +0700

    Pengumpulan Tugas Balik Kata JS - NIOMIC

--- balikKata.js
+++ balikKata.js
@@ -1,8 +1,23 @@
+console.log(
+  "BALIK KATA\n======================================================"
+);
 function balikKata(kata) {
-  return kata.split("").reverse(",").join("");
+  return kata.split("").reverse().join("");
 }
 
 console.log(balikKata("Niomic!"));
 console.log(balikKata("Javascript"));
 console.log(balikKata("alohahola"));
 console.log(balikKata("Jawa_Barat"));
+
+console.log("\natau\n======================================================");
+const balikKata2 = (kata) => {
+  let newKata = "";
+
+  for (let i = kata.length - 1; i >= 0; i--) {
+    newKata += kata[i];
+  }
+  return newKata;
+};
+
+console.log(balikKata2("Eropa"));
