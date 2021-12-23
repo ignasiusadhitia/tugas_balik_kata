7f4f28a624ada93a481d9d1c314d596dc247a458
Ignasius Yuda Adhitia <ignasius.yuda.adhitia@gmail.com>
2021-12-23 07:30:41 +0700

    Pengumpulan Tugas Balik Kata

--- balikKata.js
+++ balikKata.js
@@ -1,5 +1,5 @@
 console.log(
-  "BALIK KATA\n======================================================"
+  "BALIK KATA: SPLIT, REVERSE, DAN JOIN\n======================================================"
 );
 function balikKata(kata) {
   return kata.split("").reverse().join("");
@@ -10,7 +10,9 @@ console.log(balikKata("Javascript"));
 console.log(balikKata("alohahola"));
 console.log(balikKata("Jawa_Barat"));
 
-console.log("\natau\n======================================================");
+console.log(
+  "\nBALIK KATA: FOR\n======================================================"
+);
 const balikKata2 = (kata) => {
   let newKata = "";
 
@@ -20,4 +22,64 @@ const balikKata2 = (kata) => {
   return newKata;
 };
 
-console.log(balikKata2("Eropa"));
+console.log(balikKata2("Niomic!"));
+console.log(balikKata2("Javascript"));
+console.log(balikKata2("alohahola"));
+console.log(balikKata2("Jawa_Barat"));
+
+console.log(
+  "\nBALIK KATA: WHILE\n======================================================"
+);
+
+const balikKata3 = (kata) => {
+  let i = kata.length - 1;
+  let newKata = "";
+
+  while (i >= 0) {
+    newKata += kata[i];
+    i--;
+  }
+  return newKata;
+};
+
+console.log(balikKata3("Niomic!"));
+console.log(balikKata3("Javascript"));
+console.log(balikKata3("alohahola"));
+console.log(balikKata3("Jawa_Barat"));
+
+console.log(
+  "\nBALIK KATA: FOREACH\n======================================================"
+);
+
+const balikKata4 = (kata) => {
+  let newKata = "";
+
+  kata
+    .split("")
+    .reverse()
+    .forEach((item) => {
+      newKata += item;
+    });
+  return newKata;
+};
+
+console.log(balikKata4("Niomic!"));
+console.log(balikKata4("Javascript"));
+console.log(balikKata4("alohahola"));
+console.log(balikKata4("Jawa_Barat"));
+
+console.log(
+  "\nBALIK KATA: REDUCERIGHT\n======================================================"
+);
+
+const balikKata5 = (kata) => {
+  let newKata = "";
+
+  kata.split("").reduceRight((_, item) => (newKata += item), null);
+  return newKata;
+};
+
+console.log(balikKata5("Niomic!"));
+console.log(balikKata5("Javascript"));
+console.log(balikKata5("alohahola"));
+console.log(balikKata5("Jawa_Barat"));
